3394b049f1bc0f32f762d7ae50d9c16f04abeeb3
Schalk Neethling <sneethling@mozilla.com>
2020-06-11 18:44:26 +0200

    (fix) add logo component (#196)

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,20 +3,66 @@ const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 
 module.exports = {
   addons: [
-    "@storybook/addon-docs",
+    "@storybook/addon-actions",
     "@storybook/addon-a11y",
     "@storybook/addon-knobs",
     "@storybook/addon-essentials",
     "@storybook/addon-links",
-    "@storybook/addon-storysource"
+    "@storybook/addon-storysource",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true
+      }
+    }
   ],
   webpackFinal: async (config, { configType }) => {
-    // Allows us to import SCSS files in stories
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: [path.resolve(__dirname), path.resolve(__dirname, "..", "sass")]
+    // remove svg from existing rule
+    config.module.rules = config.module.rules.map(rule => {
+      if (
+        String(rule.test) ===
+        String(
+          /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+        )
+      ) {
+        return {
+          ...rule,
+          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/
+        };
+      }
+
+      return rule;
     });
+    // Allows us to import SCSS files in stories
+    config.module.rules.push(
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        include: [
+          path.resolve(__dirname),
+          path.resolve(__dirname, "..", "sass")
+        ]
+      },
+      {
+        test: /\.svg$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              svgo: {
+                // Disable this one svgo plugin because it
+                // strips the role attribute from our svgs
+                plugins: [{ removeUnknownsAndDefaults: false }]
+              }
+            }
+          }
+        ]
+      }
+    );
 
     // Adds https://github.com/mzgoddard/hard-source-webpack-plugin
     // for faster builds
