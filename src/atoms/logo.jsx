63800072ea80d8681920c3db3d0c46018af402dc
Schalk Neethling <sneethling@mozilla.com>
2020-07-20 11:58:01 +0200

    (component) add main menu component
    Add main menu component. This also adds some additional base configuration
    and enhancements that will benefit future work.
    
    fix #170

--- src/atoms/logo.jsx
+++ src/atoms/logo.jsx
@@ -2,6 +2,8 @@ import * as React from "react";
 
 import LogoSVG from "../icons/logo.svg";
 
+import "./logo.scss";
+
 export default function Logo() {
   return (
     <a href="/" className="logo" aria-label="MDN Web Docs">
---
+++ src/organisms/header.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { withA11y } from "@storybook/addon-a11y";
+
+import Header from "./header.jsx";
+
+export default {
+  title: "Organisms|Header",
+  decorators: [withA11y]
+};
+
+export const header = () => <Header />;
